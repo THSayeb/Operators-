2fb5849f51b04199a7842edf990329c0004241d6
THSayeb <desktop499@gmail.com>
2025-12-25 21:25:36 +0600

    Operators

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        int a= 34;
+        int b= 234;
+        int summition = a + b;
+        int difference = b - a;
+        int multiplication = a * b;
+        int divition = a / b;
+        System.out.println("Summition " + summition);
+        System.out.println("differece " + difference);
+        System.out.println("Summition " + multiplication);
+        System.out.println("differece " + divition);
+        System.out.println("------------------------------------");
+        System.out.println("Summ "+ (a+b));
+        System.out.println("Diff "+ (a-b));
+        System.out.println("Multi "+ (a*b));
+        System.out.println("Divi "+ (a/b));
+
+        System.out.println("------------------------------------");
+        System.out.println("Sum "+ (a+b) + "Diff "+ (a-b) + "Multi "+ (a*b) + "Divi "+ (a/b) );
+    }
+}
